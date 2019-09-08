51462e9c59a1dc7cd1725f48baa96377cb80b5f8
leon <leon@microsoft.com>
2019-09-08 22:21:30 +0800

    add TwoSum

---
+++ LeetCode/LeetCode/TwoSumExecutator.cs
@@ -0,0 +1,37 @@
+﻿using CommonUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class TwoSumExecutator : IExecutator
+    {
+        public void Execute()
+        {
+
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (dic.ContainsKey(complement))
+                {
+                    return new int[] { dic[complement], i };
+                }
+
+                if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
